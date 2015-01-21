88467c43a9a71c5572b3951683ba34285702151a
Claudio Marrero <cmarrero01@gmail.com>
2015-01-21 14:21:11 -0300

    Allow cros origin

--- server.js
+++ server.js
@@ -100,6 +100,32 @@ var Ya = (function(){
 	_app.set('views', __dirname + '/tpl');
 	_app.use(_bodyParser.urlencoded({ extended: false }));
 	_app.use(_bodyParser.json());
+    _app.use(function(req, res, next) {
+        var oneof = false;
+        if(req.headers.origin) {
+            res.header('Access-Control-Allow-Origin', req.headers.origin);
+            oneof = true;
+        }
+        if(req.headers['access-control-request-method']) {
+            res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+            oneof = true;
+        }
+        if(req.headers['access-control-request-headers']) {
+            res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+            oneof = true;
+        }
+        if(oneof) {
+            res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+        }
+        res.header('Access-Control-Allow-Credentials', true);
+        // intercept OPTIONS method
+        if (oneof && req.method == 'OPTIONS') {
+            res.send(200);
+        }
+        else {
+            next();
+        }
+    });
 	_app.listen(80);
 
 	/**
